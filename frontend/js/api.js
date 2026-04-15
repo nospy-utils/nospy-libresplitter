09d835d0b3823b124820d3fe57faefdc23a0e3f3
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2026-04-16 11:13:36 +1200

    remove api base so it's always relative regardless of the environment

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080';
+const API_BASE = '';
 const API_USERS_SIGNUP = API_BASE + '/api/users/signup';
 const API_USERS_ME = API_BASE + '/api/users/me';
 const API_USERS_SIGNIN = API_BASE + '/api/users/signin';
