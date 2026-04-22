a474a076b51a5177dd3991b811351cd32a9f5df3
Tatiana Morimoto <tatianamorimoto@Tatianas-MacBook-Air.local>
2026-04-22 06:16:54 -0300

    delete button

--- frontend/js/friend-detail.js
+++ frontend/js/friend-detail.js
@@ -72,14 +72,9 @@ function buildExpenseRow({from_user_name, description, currency, expense_total, 
                 </div>
             </div>
         </div>
-        <div class="col-2">
-            <div class="expense-thumbnail">
-                <img src="vendor/img/icons/receipt.svg" alt=""/>
-            </div>
-        </div>
         <div class="col">
             <div class="row">
-                <div class="col">
+                <div class="col title">
                     ${description}
                 </div>
             </div>
@@ -89,16 +84,21 @@ function buildExpenseRow({from_user_name, description, currency, expense_total, 
                 </div>
             </div>
         </div>
-        <div class="col-3 text-end">
+        <div class="col-4 text-end">
             <div class="row">
                 <div class="col">
-                    <span class="${amountClass}">${amountLabel}</span>
+                    <small class="${amountClass}">${amountLabel}</small>
                 </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <span class="${amountClass}">${currency} ${formatAmount(value)}</span>
+                    <span class="${amountClass} title">${currency} ${formatAmount(value)}</span>
                 </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="expense-bin">
+                <svg title="delete" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" enable-background="new 0 0 100 100" xml:space="preserve"><ellipse cx="40.881" cy="52.812" rx="1.875" ry="2.812"/><ellipse cx="59.631" cy="52.812" rx="1.875" ry="2.812"/><path d="M55.882,58.438c-0.519,0-0.938,0.419-0.938,0.938c0,2.585-2.102,4.688-4.687,4.688  c-2.586,0-4.688-2.102-4.688-4.688c0-0.518-0.419-0.938-0.937-0.938c-0.519,0-0.938,0.419-0.938,0.938  c0,3.618,2.944,6.562,6.563,6.562c3.618,0,6.562-2.944,6.562-6.562C56.819,58.857,56.399,58.438,55.882,58.438z"/><path d="M78.382,19.062H60.474c-0.436-2.139-2.327-3.75-4.592-3.75h-11.25c-2.266,0-4.157,1.611-4.593,3.75H22.132  c-1.551,0-2.812,1.262-2.812,2.812c0,1.24,0.811,2.283,1.928,2.655l3.699,51.786l0,0v0.002c0.176,2.459,2.363,4.62,4.683,4.622  c0.002,0,0.002,0,0.002,0c0.004,0,0.005-0.002,0.009-0.002h41.231c0.004,0,0.006,0.002,0.009,0.002c0,0,0,0,0.002,0  c2.32-0.002,4.508-2.163,4.684-4.622v-0.002l0,0l3.698-51.786c1.117-0.372,1.929-1.415,1.929-2.655  C81.194,20.324,79.932,19.062,78.382,19.062z M44.632,17.188h11.25c1.221,0,2.252,0.787,2.64,1.875h-16.53  C42.379,17.975,43.41,17.188,44.632,17.188z M73.696,76.184L73.696,76.184c0,0.002,0,0.002,0,0.002  c-0.104,1.474-1.47,2.871-2.809,2.876c-0.002,0-0.003,0-0.005,0h-41.25c-0.002,0-0.004,0-0.005,0  c-1.339-0.005-2.704-1.403-2.809-2.876c0,0,0,0,0-0.002l0,0l-3.678-51.497h54.235L73.696,76.184z M78.382,22.812h-56.25  c-0.517,0-0.938-0.421-0.938-0.938s0.421-0.938,0.938-0.938h56.25c0.516,0,0.937,0.421,0.937,0.938S78.898,22.812,78.382,22.812z"/></svg>
             </div>
         </div>
     </div>`;
