4588c8bd8bd0874af70a8e60f63febb1fe468f1c
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2026-04-26 18:11:31 +1200

    fix run.sh

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 
+envsubst '${NGINX_SERVER_NAME}' < /etc/nginx/http.d/default.template > /etc/nginx/http.d/default.conf
+nginx
+
 echo "ENV set to ${ENV}"
 if [[ "$ENV" == "production" ]]; then
     certbot --nginx --non-interactive --agree-tos --domains "${NGINX_SERVER_NAME}"
 fi
-
-envsubst '${NGINX_SERVER_NAME}' < /etc/nginx/http.d/default.template > /etc/nginx/http.d/default.conf
-nginx -g 'daemon off;'
