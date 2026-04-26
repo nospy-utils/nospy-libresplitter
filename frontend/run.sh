5f0c799471c0fa6058d03f0782ba1314bac7134f
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2026-04-26 17:59:49 +1200

    specify additional parameter to certbot

--- frontend/run.sh
+++ frontend/run.sh
@@ -2,7 +2,7 @@
 
 echo "ENV set to ${ENV}"
 if [[ "$ENV" == "production" ]]; then
-    certbot --nginx --non-interactive --agree-tos
+    certbot --nginx --non-interactive --agree-tos --domains "${NGINX_SERVER_NAME}"
 fi
 
 envsubst '${NGINX_SERVER_NAME}' < /etc/nginx/http.d/default.template > /etc/nginx/http.d/default.conf
