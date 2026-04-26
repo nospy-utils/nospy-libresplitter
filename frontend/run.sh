3ff5e7ebfe5046d1c92b57f4aa966d23594182c3
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2026-04-26 20:34:07 +1200

    change certbot to staging mode so I don't get rate limited

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,10 +1,11 @@
 #/bin/sh
 
+chmod o+r -R /usr/share/nginx/html
 envsubst '${NGINX_SERVER_NAME}' < /etc/nginx/http.d/default.template > /etc/nginx/http.d/default.conf
 
 echo "ENV set to ${ENV}"
 if [[ "$ENV" == "production" ]]; then
-    certbot --nginx --non-interactive --agree-tos --domains "${NGINX_SERVER_NAME}"
+    certbot --nginx --non-interactive --agree-tos --domains "${NGINX_SERVER_NAME}" -v --test-cert
     pkill -9 nginx
 fi
 
