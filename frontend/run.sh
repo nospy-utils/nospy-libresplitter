06aa769f50efd9069a5912b54e07cf274b14d66b
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2026-04-26 16:47:06 +1200

    attempt certbot certificate issuing (2)

--- frontend/run.sh
+++ frontend/run.sh
@@ -2,7 +2,7 @@
 
 echo "ENV set to ${ENV}"
 if [[ "$ENV" == "production" ]]; then
-    certbot --nginx
+    certbot --nginx --non-interactive --agree-tos
 fi
 
 envsubst '${NGINX_SERVER_NAME}' < /etc/nginx/http.d/default.template > /etc/nginx/http.d/default.conf
