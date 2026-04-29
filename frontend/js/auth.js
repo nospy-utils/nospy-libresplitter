375529d26dd8d85fa97904aef8fa3ba8eddfefe2
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2026-04-29 17:58:41 +1200

    redirect user if already logged in

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -2,7 +2,16 @@
     const response = await fetch(API_USERS_ME, {
         credentials: 'include',
     });
-    if (response.status === 401) {
+
+    if(window.location.pathname === '/' ||
+            window.location.pathname.startsWith('/signin.html') ||
+            window.location.pathname.startsWith('/signup.html')){
+
+        if (response.status === 200){
+            window.location.href = 'friends.html';
+        }
+    }else if (response.status === 401) {
         window.location.href = 'signin.html?unauthenticatedUser=true';
     }
+
 })();
