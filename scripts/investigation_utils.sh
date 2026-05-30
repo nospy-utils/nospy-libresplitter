0a0a04e9b7e5ff8b33a45addd905ded5c961e62f
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2026-05-30 20:22:20 +1000

    add functions to filter logs

---
+++ scripts/investigation_utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# attempting a <.*>.env is probably the most obvious thing people try to do
+# just wanted to play with JQ to recreated something homebrewed of what I would
+# get if I were using a logging DSL like splunk SPL or grafana.
+# If the question is - well, why don't you?, the answer is simple.
+#   1) Not paying any licenses in this project - mostly out of fun really
+#   2) setting up loki + grafana would require more resources than the machine I'm using has :)
+function display_attempts_dotenv(){
+  docker logs nospy-splitwise-frontend-1  \
+    | jq -Rr 'fromjson? | select(.request_uri | test(".*\\.env$"; "i"))' \
+    | jq -sr 'group_by(.remote_addr, .status)
+             | map({ remote_addr: .[0].remote_addr, status: .[0].status, count: length })
+             | .[]
+             | [.remote_addr, .status, .count]
+             | @csv'
+}
